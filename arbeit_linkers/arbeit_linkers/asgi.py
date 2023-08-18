7563e98dd12759728ed281ac40a4d8fd73199cce
GautamSavsaviya <gautamsavasaviya222@gmail.com>
2023-08-18 18:28:53 +0530

    initialize project

---
+++ arbeit_linkers/arbeit_linkers/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for arbeit_linkers project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arbeit_linkers.settings")
+
+application = get_asgi_application()
